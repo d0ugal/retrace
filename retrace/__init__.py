815efc4e65613bdac0d6091bedefe202fb12dcc8
Dougal Matthews <dougal@redhat.com>
2015-07-25 11:04:39 +0100

    Version bump and fix problems with publish

--- retrace/__init__.py
+++ retrace/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.1.0'
+__version__ = '0.2.0.dev'
 
 from retrace.decorator import retry
 from retrace.exceptions import RetraceException
--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ if sys.argv[-1] == 'publish':
     os.system("python setup.py sdist bdist_wheel")
     os.system("twine upload dist/*")
     print("You probably want to also tag the version now:")
-    print("  git tag -a {0} -m 'version {0}'".format(get_version("retrace.py")))
+    print("  git tag -a {0} -m 'version {0}'".format(get_version("retrace")))
     print("  git push --tags")
     sys.exit()
 
